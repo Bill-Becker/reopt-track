e402c05c49c3a6b53e7488258d32c36fff1a715e
bill-becker <William.Becker@nrel.gov>
2024-11-22 16:06:50 -0700

    Add serialization for requests for or adding data

---
+++ src/reopt/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import RunData
+
+class DataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RunData
+        fields = ("id", "name")
