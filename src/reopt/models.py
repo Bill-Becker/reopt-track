503dcc90a970ca183eb95dce78853b92ba3526af
bill-becker <William.Becker@nrel.gov>
2024-11-22 15:31:15 -0700

    Add initial migrations with RunData model

--- src/reopt/models.py
+++ src/reopt/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class RunData(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.id) + " - " + self.name
