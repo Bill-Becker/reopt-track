daeaf26626305f78950466165c3fc63bf08804f3
bill-becker <William.Becker@nrel.gov>
2025-02-03 22:34:24 -0700

    Update model fields

--- src/reopt/models.py
+++ src/reopt/models.py
@@ -8,21 +8,23 @@ class RunData(models.Model):
     name = models.CharField(default="", max_length=200, null=True, blank=True)
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
-    # TODO add geolocator to translate lat/long to city/country
+    # TODO add geolocator to translate lat/long to city/country of analysis here
     country = models.CharField(max_length=200, null=True, blank=True)
     city = models.CharField(max_length=200, null=True, blank=True)
     pv_size = models.FloatField(null=True, blank=True)
-    battery_size = models.FloatField(null=True, blank=True)
+    battery_energy_size = models.FloatField(null=True, blank=True)
+    battery_power_size = models.FloatField(null=True, blank=True)
 
 
 class RunMeta(models.Model):
 
     run_data = models.OneToOneField(RunData, on_delete=models.CASCADE, related_name='runmeta', null=True, blank=True)
-    webtool_uuid = models.TextField(default="", blank=True, editable=False)
-    user_uuid = models.TextField(default="", blank=True, editable=False)
-    portfolio_uuid = models.TextField(default="", blank=True, editable=False)
-    run_uuid = models.TextField(default="", blank=True, editable=False)
-    api_version = models.CharField(null=True, blank=True)
+    webtool_run = models.BooleanField(default=False, null=True, blank=True)
+    # TODO make these uuid fields UUIDFields for Django's built-in validation
+    webtool_user_uuid = models.TextField(default="", blank=True)
+    webtool_portfolio_uuid = models.TextField(default="", blank=True)
+    direct_api_run = models.BooleanField(default=False, null=True, blank=True)
+    api_run_uuid = models.TextField(default="", blank=True)
     reoptjl_version = models.CharField(null=True, blank=True)
     STATUS_CHOICES = [
         ('optimal', 'Optimal'),
@@ -31,38 +33,20 @@ class RunMeta(models.Model):
     status = models.CharField(max_length=200, choices=STATUS_CHOICES, null=True, blank=True)
     direct_reoptjl = models.BooleanField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    # TODO grab user IP address and find location
+    #user_ip_address = models.GenericIPAddressField(null=True, blank=True)
+    #user_country = models.TextField(default="", blank=True)
 
     def clean(self, *args, **kwargs):
-        if self.webtool_uuid == "" and self.run_uuid == "":
+        if not self.webtool_run and not self.direct_api_run:
             self.direct_reoptjl = True
-        elif self.webtool_uuid != "" or self.run_uuid != "":
+        elif self.webtool_run or self.direct_api_run:
             self.direct_reoptjl = False
         else:
             # This can't actually happen based on conditionals above
             raise ValidationError("Invalid state for direct_reoptjl")
         super(RunMeta, self).clean(*args, **kwargs)
 
-
-# TODO get user's location from IP address contained in headers (maybe), but this may require permissions
-# Google Geolocation API (not the one below) is subscription based, but NREL might have an account?
-# def get_user_location(ip_address):
-#     api_key = "YOUR_API_KEY"  # Replace with your geolocation API key
-#     url = f"https://api.ipgeolocation.io/v1/YOUR_API_KEY?apiKey={api_key}&ip={ip_address}"
-#     response = requests.get(url)
-#     location_data = response.json()
-
-#     return location_data
-
-# def get_user_ip(request):
-#     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-#     if x_forwarded_for:
-#         ip = x_forwarded_for.split(',')[0]
-#     else:
-#         ip = request.META.get('REMOTE_ADDR')
-#     return ip
-
-# def get_user_location_from_request(request):
-#     ip = get_user_ip(request)
-#     location_data = get_user_location(ip)
-#     print(f"User location: {location_data['city']}, {location_data['country']}")
-#     return location_data
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(RunMeta, self).save(*args, **kwargs)
