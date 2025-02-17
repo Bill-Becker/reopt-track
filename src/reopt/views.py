3b283c64bfb8d6931ccc6647fa5d7899a4000ddf
bill-becker <William.Becker@nrel.gov>
2025-02-16 22:56:45 -0700

    Format after avoid update data on refresh

--- src/reopt/views.py
+++ src/reopt/views.py
@@ -21,25 +21,29 @@ from .serializer import RunMetaSerializer
 
 
 def dashboard(request):
-    data_dir = os.path.join(settings.BASE_DIR, 'reopt', 'data')
-    user_chart_data_path = os.path.join(data_dir, 'user_chart_data.json')
-    run_chart_data_path = os.path.join(data_dir, 'run_chart_data.json')
-    track_data_path = os.path.join(data_dir, 'track_data.json')
+    data_dir = os.path.join(settings.BASE_DIR, "reopt", "data")
+    user_chart_data_path = os.path.join(data_dir, "user_chart_data.json")
+    run_chart_data_path = os.path.join(data_dir, "run_chart_data.json")
+    track_data_path = os.path.join(data_dir, "track_data.json")
 
     # Check if any of the data files do not exist
-    if not (os.path.exists(user_chart_data_path) and os.path.exists(run_chart_data_path) and os.path.exists(track_data_path)):
+    if not (
+        os.path.exists(user_chart_data_path)
+        and os.path.exists(run_chart_data_path)
+        and os.path.exists(track_data_path)
+    ):
         # Call the update_chart_data view to generate the data files
         update_chart_data(request)
         # Redirect back to the dashboard
-        return redirect('reopt_dashboard')
+        return redirect("reopt_dashboard")
 
-    with open(os.path.join(data_dir, 'user_chart_data.json'), 'r') as f:
+    with open(os.path.join(data_dir, "user_chart_data.json"), "r") as f:
         user_chart_data = json.load(f)
 
-    with open(os.path.join(data_dir, 'run_chart_data.json'), 'r') as f:
+    with open(os.path.join(data_dir, "run_chart_data.json"), "r") as f:
         run_chart_data = json.load(f)
 
-    with open(os.path.join(data_dir, 'track_data.json'), 'r') as f:
+    with open(os.path.join(data_dir, "track_data.json"), "r") as f:
         track_data = json.load(f)
 
     user_locations_map_path = get_user_locations_map()
@@ -61,19 +65,19 @@ def update_chart_data(request):
     run_chart_data = get_run_chart_data_from_api_gov()
     track_data = get_run_counts_from_track_db()
 
-    data_dir = os.path.join(settings.BASE_DIR, 'reopt', 'data')
+    data_dir = os.path.join(settings.BASE_DIR, "reopt", "data")
     os.makedirs(data_dir, exist_ok=True)
 
-    with open(os.path.join(data_dir, 'user_chart_data.json'), 'w') as f:
+    with open(os.path.join(data_dir, "user_chart_data.json"), "w") as f:
         json.dump(user_chart_data, f)
 
-    with open(os.path.join(data_dir, 'run_chart_data.json'), 'w') as f:
+    with open(os.path.join(data_dir, "run_chart_data.json"), "w") as f:
         json.dump(run_chart_data, f)
 
-    with open(os.path.join(data_dir, 'track_data.json'), 'w') as f:
+    with open(os.path.join(data_dir, "track_data.json"), "w") as f:
         json.dump(track_data, f)
 
-    return JsonResponse({'status': 'success'})
+    return JsonResponse({"status": "success"})
 
 
 @api_view(["GET"])
